5de886b4eb68f0f7ab9d01ddb1f7885099ab6a21
mtwoo2 <mtwoo2@student.monash.edu>
2018-08-15 11:51:01 +0800

    Preprocessing and tf.records done

--- inputimages.py
+++ inputimages.py
@@ -69,7 +69,6 @@ def data_preprocessing(addrs, labels):
     
 def createDataRecord(out_filename, addrs, labels, mode):
     # open the TFRecords file
-    prob = 0.005
     imageCounter = 0
     writer = tf.python_io.TFRecordWriter(out_filename)
     for i in range(len(addrs)):
@@ -77,37 +76,38 @@ def createDataRecord(out_filename, addrs, labels, mode):
         if not i % 1000:
             print('Train data: {}/{}'.format(i, len(addrs)))
             sys.stdout.flush()
+
         # Load the image
         img = load_image(addrs[i])
 
         #img_name, extension = os.path.splitext(addrs[i])
-        img_name, extension = os.path.splitext(os.path.basename(addrs[i]))
-        path_name = os.path.dirname(addrs[i])
-        print(path_name)
+##        img_name, extension = os.path.splitext(os.path.basename(addrs[i]))
+##        path_name = os.path.dirname(addrs[i])
+##        print(path_name)
         #print(os.path.dirname(addrs[i]))
         #print(img_name)
-
-        if mode == 'train':
-            #rotate image
-            for j in range(3):
-                if j != 1:
-                    rotate = np.rot90(img, k=1+j)
-                    cv2.imwrite(os.path.join(path_name, img_name +'_R' + str(j) + '.jpg' ), rotate)
-                    label = labels[i]
-                    if rotate is None:
-                        continue
-                     # Create a feature
-                    feature = {
-                        'image_raw': _bytes_feature(rotate.tostring()),
-                        'label': _int64_feature(label)
-                    }
-                    imageCounter += 1
-                    
-                    # Create an example protocol buffer
-                    example = tf.train.Example(features=tf.train.Features(feature=feature))
-                    
-                    # Serialize to string and write on the file
-                    writer.write(example.SerializeToString())
+##
+##        if mode == 'train':
+##            #rotate image
+##            for j in range(3):
+##                if j != 1:
+##                    rotate = np.rot90(img, k=1+j)
+##                    cv2.imwrite(os.path.join(path_name, img_name +'_R' + str(j) + '.jpg' ), rotate)
+##                    label = labels[i]
+##                    if rotate is None:
+##                        continue
+##                     # Create a feature
+##                    feature = {
+##                        'image_raw': _bytes_feature(rotate.tostring()),
+##                        'label': _int64_feature(label)
+##                    }
+##                    imageCounter += 1
+##                    
+##                    # Create an example protocol buffer
+##                    example = tf.train.Example(features=tf.train.Features(feature=feature))
+##                    
+##                    # Serialize to string and write on the file
+##                    writer.write(example.SerializeToString())
 
 ##            output = np.zeros(img.shape, np.uint8)
 ##            threshold =  1 - prob
@@ -121,29 +121,29 @@ def createDataRecord(out_filename, addrs, labels, mode):
 ##                    else:
 ##                        output[i][j] = img[i][j]
                     
-            s_p = salt_pepper(img, prob)
-
-            cv2.imwrite(os.path.join(path_name, img_name +'_SP' + '.jpg' ), s_p)
-
-            #print(s_p)
-            label = labels[i]
-                        
-            if s_p is None:
-                continue
-            imageCounter += 1
-            
-            #Create a feature
-            feature = {
-                'image_raw': _bytes_feature(s_p.tostring()),
-                'label': _int64_feature(label)
-            }
-
-            #Create an example protocol buffer
-            example =  tf.train.Example(features=tf.train.Features(feature=feature))
-
-            # Serialize to string and write on the file
-            writer.write(example.SerializeToString())
-            
+##            s_p = salt_pepper(img, prob)
+##
+##            cv2.imwrite(os.path.join(path_name, img_name +'_SP' + '.jpg' ), s_p)
+##
+##            #print(s_p)
+##            label = labels[i]
+##                        
+##            if s_p is None:
+##                continue
+##            imageCounter += 1
+##            
+##            #Create a feature
+##            feature = {
+##                'image_raw': _bytes_feature(s_p.tostring()),
+##                'label': _int64_feature(label)
+##            }
+##
+##            #Create an example protocol buffer
+##            example =  tf.train.Example(features=tf.train.Features(feature=feature))
+##
+##            # Serialize to string and write on the file
+##            writer.write(example.SerializeToString())
+##            
             
         
         label = labels[i]
@@ -216,15 +216,15 @@ print(len(labels))
 
 data_preprocessing(addrs, labels)
 
-### Divide the data into 70% train, 30% test
-##train_addrs = addrs[0:int(0.7*len(addrs))]
-##train_labels = labels[0:int(0.7*len(labels))]
-###val_addrs = addrs[int(0.6*len(addrs)):int(0.8*len(addrs))]
-###val_labels = labels[int(0.6*len(addrs)):int(0.8*len(addrs))]
-##test_addrs = addrs[int(0.7*len(addrs)):]
-##test_labels = labels[int(0.7*len(labels)):]
-##
-##createDataRecord('train.tfrecords', train_addrs, train_labels, 'train')
-###createDataRecord('val.tfrecords', val_addrs, val_labels)
-##createDataRecord('test.tfrecords', test_addrs, test_labels, 'test')
+# Divide the data into 70% train, 30% test
+train_addrs = addrs[0:int(0.7*len(addrs))]
+train_labels = labels[0:int(0.7*len(labels))]
+#val_addrs = addrs[int(0.6*len(addrs)):int(0.8*len(addrs))]
+#val_labels = labels[int(0.6*len(addrs)):int(0.8*len(addrs))]
+test_addrs = addrs[int(0.7*len(addrs)):]
+test_labels = labels[int(0.7*len(labels)):]
+
+createDataRecord('train.tfrecords', train_addrs, train_labels, 'train')
+#createDataRecord('val.tfrecords', val_addrs, val_labels)
+createDataRecord('test.tfrecords', test_addrs, test_labels, 'test')
     
